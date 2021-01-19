24f3b6c2609cdc292fe3d1c34de70dba9cae38ec
Em <emilylauder@gmail.com>
2021-01-18 19:13:05 -0500

    Updating schema

--- models/workout.js
+++ models/workout.js
@@ -4,30 +4,32 @@ const Schema = mongoose.schema;
 
 const workoutSchema = new Schema({
   // name, type, weight, sets, reps, and duration
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for your workout",
+  day: {
+    type: Date,
+    default: new Date(),
   },
-  type: {
-    type: String,
-    trim: true,
-    required: "Enter a type of workout",
-  },
-  weight: {
-    type: Number,
-  },
-  sets: {
-    type: Number,
-    required: "Enter the number of sets you did during your workout",
-  },
-  reps: {
-    type: Number,
-    required: "Enter the number of reps you did during your workout",
-  },
-  duration: {
-    type: Number,
-  },
+  exercise: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Your exercise type is required.",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Exercise name is required.",
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      distance: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
