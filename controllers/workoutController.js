17950adb88712b7220a11736c7a1ad38c6d72d73
Em <emilylauder@gmail.com>
2021-01-18 19:39:58 -0500

    workout controller add get routes

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -5,3 +5,35 @@ const router = express.Router();
 const db = require("../models");
 
 // API Routes
+
+// find all workouts
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((workouts) => {
+      res.json(workouts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to retrieve workouts.",
+      });
+    });
+});
+
+// find one workout
+router.get("/api/workouts/:id", (req, res) => {
+  db.Workout.findById(req.params.id)
+    .then((workouts) => {
+      res.json(workouts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: `Failed to retrieve workout: ${req.params.id}`,
+      });
+    });
+});
