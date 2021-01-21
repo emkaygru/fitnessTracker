dda6299ad5974a015ff7deb0d593912e0f92f5e9
Em <emilylauder@gmail.com>
2021-01-20 21:06:35 -0500

    created apiRoutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,62 @@
+// dependencies
+const router = require("express").Router();
+const db = require("../models");
+
+// GET route that retrieves workouts from the database
+router.get("api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// POST route => adds new workout to the database
+router.post("api/workouts", (req, res) => {
+  db.Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// PUT route => updates workouts in the database
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: {
+        exercises: body,
+      },
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// GET route => gets range of workouts from database
+router.get("api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .limit(7)
+    .then((dbWorkout) => {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
